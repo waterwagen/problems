ba8f3216c2478c6da54b86affb13979a7e5dbdf6
Josh Taylor <joshuataylor@beyond.com>
2025-01-11 20:46:14 -0700

    New solution and testing for two sum problem

--- lib/src/test/java/com/waterwagen/TwoSum2InputArrayIsSortedTest.java
+++ lib/src/test/java/com/waterwagen/TwoSum2InputArrayIsSortedTest.java
@@ -4,9 +4,54 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
+import java.util.Arrays;
+import java.util.Random;
+
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class TwoSum2InputArrayIsSortedTest {
+
+  @Test
+  public void test_Large_MapLookup() {
+    test_Large(Study.TwoSumIIInputArrayIsSortedSolution::twoSumMapLookup);
+  }
+
+  @Test
+  @Disabled
+  public void test_Large_Naive() {
+    test_Large(Study.TwoSumIIInputArrayIsSortedSolution::twoSumNaive);
+  }
+
+  @Test
+  public void test_Large_DivideAndConquer() {
+    test_Large(Study.TwoSumIIInputArrayIsSortedSolution::twoSumDivideAndConquer);
+  }
+
+  private void test_Large(TwoSumCall twoSumCall) {
+    // given
+    long totalExecutionTime = 0;
+    int iterationLimit = 100;
+    for (int iteration = 0; iteration < iterationLimit; iteration++) {
+      int numCount = 1_000_000; // set this to 10_000_000 to clearly see differences between map lookup and divide & conquer
+      int[] nums = new int[numCount];
+      Random random = new Random();
+      for (int index = 0; index < numCount; index++) {
+        nums[index] = random.nextInt(100_000);
+      }
+      Arrays.sort(nums);
+      int firstTargetIndex = random.nextInt(numCount);
+      int secondTargetIndex = random.nextInt(numCount);
+      while (secondTargetIndex == firstTargetIndex) {
+        secondTargetIndex = random.nextInt(numCount);
+      }
+      int target = nums[firstTargetIndex] + nums[secondTargetIndex];
+
+      totalExecutionTime += testTwoSumLarge(nums, target, twoSumCall);
+    }
+    System.out.println("-----------------------------------------------------------------");
+    System.out.println("Avg execution time: " + totalExecutionTime/iterationLimit + "ms");
+  }
 
   @Test
   public void test_Adjacent_Positive() {
@@ -80,10 +125,33 @@ public class TwoSum2InputArrayIsSortedTest {
 
   private static void testTwoSum(int[] nums, int target, int[] expectedIndices) {
     // when
-    int[] indices = Study.TwoSumIIInputArrayIsSortedSolution.twoSum(nums, target);
+    int[] indices = Study.TwoSumIIInputArrayIsSortedSolution.twoSumMapLookup(nums, target);
 
     // then
     assertThat(indices, is(equalTo(expectedIndices)));
+  }
+
+  private static long testTwoSumLarge(int[] nums, int target, TwoSumCall twoSumCall) {
+    // when
+    long startTime = System.nanoTime();
+    int[] indices = twoSumCall.getTwoSum(nums, target);
+    long endTime = System.nanoTime();
+    System.out.println("Target: " + target);
+    System.out.println("Large two sum indices: " + Arrays.toString(indices));
+    if (indices.length > 0)
+      System.out.println("Large two sum values: " + nums[indices[0] - 1] + "," + nums[indices[1] - 1]);
+    long executionTime = (endTime - startTime)/1_000_000;
+    System.out.println("Large two sum execution time: " + executionTime + "ms");
+
+    // then
+    assertThat(nums[indices[0] - 1] + nums[indices[1] - 1], is(equalTo(target)));
+
+    return executionTime;
+  }
+
+  @FunctionalInterface
+  private interface TwoSumCall {
+    int[] getTwoSum(int[] nums, int target);
   }
 
 }
